d789ea2a6ed83fe952c5dbc6c92e86b92d3cbfae
Hoyoung Kim <ghdud9606@gmail.com>
2020-08-15 14:04:32 +0900

    Add code Todo Input

--- vue-intermediate/vue-todo/src/components/TodoInput.vue
+++ vue-intermediate/vue-todo/src/components/TodoInput.vue
@@ -1,15 +1,59 @@
 <template>
-  <div>
-    input
+  <div class="inputBox shadow">
+    <input type="text" v-model="newToDoItem" v-on:keyup.enter="addTodo">
+    <!-- <button v-on:click="addTodo">Add</button> -->
+    <span class="addContainer" v-on:click="addTodo">
+      <i class="addBtn fas fa-plus"></i>
+    </span>
   </div>
 </template>
 
 <script>
 export default {
-
+data: function() {
+  return {
+    newToDoItem: ""
+  }
+},
+methods: {
+  addTodo: function() {
+    console.log(this.newToDoItem)
+    localStorage.setItem(this.newToDoItem, this.newToDoItem)
+    this.clearInput()
+  },
+  clearInput: function() {
+    this.newToDoItem = ''
+  }
+}
 }
 </script>
 
-<style>
-
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+.closeModalBtn {
+  color: #42b983;
+}
 </style>
