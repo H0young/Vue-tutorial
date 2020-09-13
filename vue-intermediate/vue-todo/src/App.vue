58f15f8b1cb43702f3f6ad4587e4d6f9b42339be
Hoyoung Kim <ghdud9606@gmail.com>
2020-09-13 18:14:56 +0900

    Refactory: state 속성 적용

--- vue-intermediate/vue-todo/src/App.vue
+++ vue-intermediate/vue-todo/src/App.vue
@@ -1,62 +1,56 @@
 <template>
-   <div id="app">
-     <TodoHeader></TodoHeader>
-     <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
-     <TodoList v-bind:propsdata="todoItems" v-on:removeItem="removeOneItem" v-on:toggleItem="toggleOneItem"></TodoList>
-     <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
-   </div>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
+    <TodoList
+      v-bind:propsdata="todoItems"
+      v-on:removeItem="removeOneItem"
+      v-on:toggleItem="toggleOneItem"
+    ></TodoList>
+    <TodoFooter v-on:clearAll="clearAllItems"></TodoFooter>
+  </div>
 </template>
 
 <script>
-import TodoHeader from './components/TodoHeader.vue'
-import TodoInput from './components/TodoInput.vue'
-import TodoList from './components/TodoList.vue'
-import TodoFooter from './components/TodoFooter.vue'
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
   data: function() {
     return {
       todoItems: []
-    }
+    };
   },
   methods: {
     addOneItem: function(todoItem) {
-      const obj = {completed: false, item: todoItem}
-      localStorage.setItem(todoItem, JSON.stringify(obj))
-      this.todoItems.push(obj)
+      const obj = { completed: false, item: todoItem };
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      this.todoItems.push(obj);
     },
     removeOneItem: function(todoItem, index) {
-      this.todoItems.splice(index, 1)
-      localStorage.removeItem(todoItem.item)
+      this.todoItems.splice(index, 1);
+      localStorage.removeItem(todoItem.item);
     },
     toggleOneItem: function(todoItem, index) {
-      this.todoItems[index].completed = !this.todoItems[index].completed
-          // 로컬스토리지 데이터 갱신
-      localStorage.removeItem(todoItem.item)
-      localStorage.setItem(todoItem.item, JSON.stringify(todoItem))
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+      // 로컬스토리지 데이터 갱신
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     },
     clearAllItems: function() {
-      localStorage.clear()
-      this.todoItems = []
-    }
-  },
-  created: function() {
-    if (localStorage.length > 0) {
-      for (let i = 0; i < localStorage.length ; i ++) {
-        if (localStorage.key(i) !== 'loglevel:webpack-dev-server') {
-          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))))
-        }
-      }
+      localStorage.clear();
+      this.todoItems = [];
     }
   },
   components: {
-    'TodoHeader': TodoHeader,
-    'TodoInput': TodoInput,
-    'TodoList': TodoList,
-    'TodoFooter': TodoFooter
+    TodoHeader: TodoHeader,
+    TodoInput: TodoInput,
+    TodoList: TodoList,
+    TodoFooter: TodoFooter
   }
-
-}
+};
 </script>
 
 <style>
--- vue-intermediate/vue-todo/src/components/TodoList.vue
+++ vue-intermediate/vue-todo/src/components/TodoList.vue
@@ -1,29 +1,39 @@
 <template>
   <div>
-  <ul> 
-    <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
-      <i class="checkBtn fas fa-check" v-bind:class="{checkBtnCompleted: todoItem.completed}" v-on:click="toggleComplete(todoItem, index)"></i>
-      <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
-      <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
-        <i class="fas fa-trash-alt"></i>
-      </span>
-    </li>
-  </ul>
+    <ul>
+      <li
+        v-for="(todoItem, index) in this.$store.state.todoItems"
+        v-bind:key="todoItem.item"
+        class="shadow"
+      >
+        <i
+          class="checkBtn fas fa-check"
+          v-bind:class="{ checkBtnCompleted: todoItem.completed }"
+          v-on:click="toggleComplete(todoItem, index)"
+        ></i>
+        <span v-bind:class="{ textCompleted: todoItem.completed }">{{
+          todoItem.item
+        }}</span>
+        <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
+          <i class="fas fa-trash-alt"></i>
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['propsdata'],
+  props: ["propsdata"],
   methods: {
     removeTodo: function(todoItem, index) {
-      this.$emit('removeItem', todoItem, index)
+      this.$emit("removeItem", todoItem, index);
     },
     toggleComplete: function(todoItem, index) {
-      this.$emit('toggleItem', todoItem, index)
+      this.$emit("toggleItem", todoItem, index);
     }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -61,7 +71,8 @@ li {
 }
 
 /* transition css */
-.list-enter-active, .list-leave-active {
+.list-enter-active,
+.list-leave-active {
   transition: all 1s;
 }
 .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
